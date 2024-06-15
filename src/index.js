070762e37b585546158ae67a941ab5f4c1c86493
Sarita200 <saritasadgir1@gmail.com>
2024-06-15 18:47:38 +0530

    ASS40 completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-import ReactDom from "react-dom/client"
-const root=ReactDom.createRoot(document.getElementById("root"))
-root.render(<h1>Hello react..!</h1>)
+import ReactDOM from "react-dom/client"
+import {
+    createBrowserRouter,
+    RouterProvider
+} from "react-router-dom";
+
+import Home from "./views/Home/Home";
+import About from "./views/About/About";
+import Contact from "./views/Contact/Contact";
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const router = createBrowserRouter([
+    {
+        path : "/",
+        element : <Home/>
+    },
+    {
+        path : "/about",
+        element : <About/>
+    },
+    {
+        path : "/contact",
+        element : <Contact/>
+    }
+])
+root.render(<RouterProvider router={router}/>)
+ 
